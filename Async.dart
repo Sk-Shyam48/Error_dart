07976f9c276aae8aa60311c5a38d81aa95ad13ae
Sk-Shyam48 <Skshyam48484@gmail.com>
2024-04-11 17:16:03 +0530

    verified

--- Async.dart
+++ Async.dart
@@ -6,19 +6,24 @@ class UserData {
 
   UserData(this.id, this.name);
 }
-
+// asynchronous function to fetch user data
 Future<UserData> fetchUserData(int userId, String username) async {
+  // delay in fetching user data
   await Future.delayed(Duration(seconds: 2));
   return UserData("User Id   : $userId", "User Name : $username");
 }
 
 void main() async {
+  // test fetching user data
   print("Fetching user data...");
 
   try {
+    // asynchronously and await the result
     UserData userData = await fetchUserData(1001, "WHite_DEvil");
     print("User Data  ↓ \n ${userData.id} \n ${userData.name}");
-  } catch (error) {
+  }
+  // any exceptions
+  catch (error) {
     print("Error fetching user data: $error");
   }
 }
